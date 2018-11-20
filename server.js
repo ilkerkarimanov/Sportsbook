f9d33e6c1e48e4045e1e57bc049aca19051ae437
Ilker Karimanov <ilker.karimanov@hotmail.com>
2018-11-20 13:03:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.get('/matches', async (req, res) => {
-    const data = await fetch('http://sports.ultraplay.net/sportsxml?siteID=1&sportId=2357').then(response => response.text())
+    const data = await fetch('https://sports.ultraplay.net/sportsxml?clientKey=1bf3f918-fa27-4400-8815-d14a130f6851&sportId=2357').then(response => response.text())
 
     xml2js(data, (err, result) => {
         if (err) {
